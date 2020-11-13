bf32f6b29bd511cd8b5918155d1b58496cda560f
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2020-11-13 03:45:05 -0600

    Add basic messaging between tienda and socio

--- socio.erl
+++ socio.erl
@@ -1,1 +1,27 @@
--module(socio)
+-module(socio).
+-export([suscribir_socio/1]).
+
+% nombre largo del servidor (nombre@máquina)
+matriz() -> 'tienda@Inakis-MacBook-Pro'.
+
+% funciones de interfase
+% consulta(Quien) ->
+%     llama_banco({consulta, Quien}).
+% deposita(Quien, Cantidad) ->
+%     llama_banco({deposita, Quien, Cantidad}).
+% retira(Quien, Cantidad) ->
+%     llama_banco({retira, Quien, Cantidad}).
+% % cliente
+suscribir_socio(Socio) ->
+    Matriz = matriz(),
+    monitor_node(Matriz, true),
+    {servidor_tienda, Matriz} ! {self(), mensaje},
+    receive
+        {servidor_banco, Respuesta} ->
+            monitor_node(Matriz, false),
+            Respuesta;
+        {nodedown, Matriz} ->
+            no;
+        PorLoProntoParaProbar ->
+            io:format("Mensaje recibido: ~p~n", [PorLoProntoParaProbar])
+    end.
--- tienda.erl
+++ tienda.erl
@@ -1,6 +1,17 @@
--module(tienda)
+-module(tienda).
+-export([init_tienda/0, tienda/0]).
 
-tienda(Datos) ->
 
-.
+init_tienda() ->
+    register(servidor_tienda,
+        spawn('tienda@Inakis-MacBook-Pro', fun tienda/0)).
+tienda() ->
+    receive
+        {PID, _} ->
+            io:format("Hola, soy la tienda ~n"),
+            PID ! "¡Mensaje llego bien a tienda!",
+            tienda();
+        _ -> 
+            io:format("Mensaje incorrecto ~n")
 
+    end.
